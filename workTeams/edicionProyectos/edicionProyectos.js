0d85da73965092f34fbb01fca2462a669c0ae78a
Alucard1453 <djorgealbertola@gmail.com>
2020-06-09 14:38:53 -0500

    Al loguearse se muestran los proyectos del usuario, cambio simple en base de datos

---
+++ workTeams/edicionProyectos/edicionProyectos.js
@@ -0,0 +1,16 @@
+$(document).ready(function (){
+    validarsesion();
+
+    //Funcion para inicializar boton de menu izquierdo
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+});
+
+//Valida si hay una sesion iniciada
+function validarsesion(){
+    if(!localStorage.getItem("Logueado")){
+        location.href ="../login/";
+    }
+}
