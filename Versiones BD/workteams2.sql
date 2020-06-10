03267b14d32ae308e072c7e7785da1e223853c71
Esteban <shirou.k7@gmail.com>
2020-06-10 00:33:43 -0500

    Agregar Historias de Usuario

---
+++ Versiones BD/workteams2.sql
@@ -0,0 +1,420 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 10-06-2020 a las 03:13:54
+-- Versión del servidor: 10.1.29-MariaDB
+-- Versión de PHP: 7.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `workteams`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `calificacionusuario`
+--
+
+CREATE TABLE `calificacionusuario` (
+  `idCalificacion` int(11) NOT NULL,
+  `idProyecto` int(11) NOT NULL,
+  `idCalificador` int(11) NOT NULL,
+  `idCalificado` int(11) NOT NULL,
+  `calificacion` int(11) NOT NULL,
+  `comentario` varchar(200) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comentariosproyecto`
+--
+
+CREATE TABLE `comentariosproyecto` (
+  `idComPro` int(11) NOT NULL,
+  `idProyecto` int(11) NOT NULL,
+  `fechaHora` varchar(22) COLLATE utf8_spanish_ci NOT NULL,
+  `comentario` varchar(500) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comentariosrelease`
+--
+
+CREATE TABLE `comentariosrelease` (
+  `idComentario` int(11) NOT NULL,
+  `idRelease` int(11) NOT NULL,
+  `fechaHora` varchar(22) COLLATE utf8_spanish_ci NOT NULL,
+  `comentario` varchar(500) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estado`
+--
+
+CREATE TABLE `estado` (
+  `idEstado` int(11) NOT NULL,
+  `nomEstado` varchar(20) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `estado`
+--
+
+INSERT INTO `estado` (`idEstado`, `nomEstado`) VALUES
+(1, 'Sin Asignar'),
+(2, 'En Curso'),
+(3, 'Finalizado');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `participantes`
+--
+
+CREATE TABLE `participantes` (
+  `idProyecto` int(11) NOT NULL,
+  `idUsuario` int(11) NOT NULL,
+  `rol` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proyecto`
+--
+
+CREATE TABLE `proyecto` (
+  `idProyecto` int(11) NOT NULL,
+  `propietario` int(11) NOT NULL,
+  `nombre` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `descripcion` varchar(512) COLLATE utf8_spanish_ci NOT NULL,
+  `fechaInicio` varchar(12) COLLATE utf8_spanish_ci NOT NULL,
+  `fechaFin` varchar(12) COLLATE utf8_spanish_ci NOT NULL,
+  `numRelease` int(11) NOT NULL,
+  `estado` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `proyecto`
+--
+
+INSERT INTO `proyecto` (`idProyecto`, `propietario`, `nombre`, `descripcion`, `fechaInicio`, `fechaFin`, `numRelease`, `estado`) VALUES
+(1, 13, 'Proyecto Prueba', 'Proyecto para desarrollar', '09/06/2020', '12/12/2020', 10, 1),
+(2, 13, 'Prueba dos', 'Proyecto para desarrollar con todo', '05/05/2020', '10/09/2020', 15, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `release`
+--
+
+CREATE TABLE `release` (
+  `idRelease` int(11) NOT NULL,
+  `idProyecto` int(11) NOT NULL,
+  `fechaInicio` varchar(12) COLLATE utf8_spanish_ci NOT NULL,
+  `fechaFin` varchar(12) COLLATE utf8_spanish_ci NOT NULL,
+  `estado` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rol`
+--
+
+CREATE TABLE `rol` (
+  `idRol` int(11) NOT NULL,
+  `nombreRol` varchar(12) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `rol`
+--
+
+INSERT INTO `rol` (`idRol`, `nombreRol`) VALUES
+(1, 'Lider'),
+(2, 'Stakeholder'),
+(3, 'Programador');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tarea`
+--
+
+CREATE TABLE `tarea` (
+  `idTarea` int(11) NOT NULL,
+  `idProyecto` int(11) NOT NULL,
+  `nombreTarea` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `numTarea` int(11) NOT NULL,
+  `descripcion` varchar(300) COLLATE utf8_spanish_ci NOT NULL,
+  `asignado` int(11) DEFAULT NULL,
+  `estado` int(11) NOT NULL,
+  `puntos` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `tarea`
+--
+
+INSERT INTO `tarea` (`idTarea`, `idProyecto`, `nombreTarea`, `numTarea`, `descripcion`, `asignado`, `estado`, `puntos`) VALUES
+(1, 2, 'Tarea 1', 1, 'Descripción Tarea 1', 0, 1, 5),
+(2, 2, 'Tarea 2', 2, 'Descripción 2', NULL, 1, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tareasrelease`
+--
+
+CREATE TABLE `tareasrelease` (
+  `idRelease` int(11) NOT NULL,
+  `idTarea` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `idUsuario` int(11) NOT NULL,
+  `nombre` varchar(25) COLLATE utf8_spanish_ci NOT NULL,
+  `aPaterno` varchar(25) COLLATE utf8_spanish_ci NOT NULL,
+  `aMaterno` varchar(25) COLLATE utf8_spanish_ci NOT NULL,
+  `usuario` varchar(15) COLLATE utf8_spanish_ci NOT NULL,
+  `contrasena` varchar(12) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`idUsuario`, `nombre`, `aPaterno`, `aMaterno`, `usuario`, `contrasena`) VALUES
+(13, 'Jorge Alberto', 'López', 'Aragón', 'jlopeza', '123456');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `calificacionusuario`
+--
+ALTER TABLE `calificacionusuario`
+  ADD PRIMARY KEY (`idCalificacion`),
+  ADD KEY `idProyecto` (`idProyecto`),
+  ADD KEY `idCalificador` (`idCalificador`),
+  ADD KEY `idCalificado` (`idCalificado`);
+
+--
+-- Indices de la tabla `comentariosproyecto`
+--
+ALTER TABLE `comentariosproyecto`
+  ADD PRIMARY KEY (`idComPro`),
+  ADD KEY `idProyecto` (`idProyecto`);
+
+--
+-- Indices de la tabla `comentariosrelease`
+--
+ALTER TABLE `comentariosrelease`
+  ADD PRIMARY KEY (`idComentario`),
+  ADD KEY `idRelease` (`idRelease`);
+
+--
+-- Indices de la tabla `estado`
+--
+ALTER TABLE `estado`
+  ADD PRIMARY KEY (`idEstado`);
+
+--
+-- Indices de la tabla `participantes`
+--
+ALTER TABLE `participantes`
+  ADD KEY `idProyecto` (`idProyecto`),
+  ADD KEY `idUsuario` (`idUsuario`),
+  ADD KEY `rol` (`rol`);
+
+--
+-- Indices de la tabla `proyecto`
+--
+ALTER TABLE `proyecto`
+  ADD PRIMARY KEY (`idProyecto`),
+  ADD KEY `propietario` (`propietario`),
+  ADD KEY `estado` (`estado`);
+
+--
+-- Indices de la tabla `release`
+--
+ALTER TABLE `release`
+  ADD PRIMARY KEY (`idRelease`),
+  ADD KEY `idProyecto` (`idProyecto`),
+  ADD KEY `estado` (`estado`);
+
+--
+-- Indices de la tabla `rol`
+--
+ALTER TABLE `rol`
+  ADD PRIMARY KEY (`idRol`);
+
+--
+-- Indices de la tabla `tarea`
+--
+ALTER TABLE `tarea`
+  ADD PRIMARY KEY (`idTarea`),
+  ADD KEY `idProyecto` (`idProyecto`),
+  ADD KEY `estado` (`estado`);
+
+--
+-- Indices de la tabla `tareasrelease`
+--
+ALTER TABLE `tareasrelease`
+  ADD KEY `idRelease` (`idRelease`),
+  ADD KEY `idTarea` (`idTarea`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`idUsuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `calificacionusuario`
+--
+ALTER TABLE `calificacionusuario`
+  MODIFY `idCalificacion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `comentariosproyecto`
+--
+ALTER TABLE `comentariosproyecto`
+  MODIFY `idComPro` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `comentariosrelease`
+--
+ALTER TABLE `comentariosrelease`
+  MODIFY `idComentario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `estado`
+--
+ALTER TABLE `estado`
+  MODIFY `idEstado` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `proyecto`
+--
+ALTER TABLE `proyecto`
+  MODIFY `idProyecto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `release`
+--
+ALTER TABLE `release`
+  MODIFY `idRelease` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `rol`
+--
+ALTER TABLE `rol`
+  MODIFY `idRol` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `tarea`
+--
+ALTER TABLE `tarea`
+  MODIFY `idTarea` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `calificacionusuario`
+--
+ALTER TABLE `calificacionusuario`
+  ADD CONSTRAINT `calificacionusuario_ibfk_1` FOREIGN KEY (`idProyecto`) REFERENCES `proyecto` (`idProyecto`),
+  ADD CONSTRAINT `calificacionusuario_ibfk_2` FOREIGN KEY (`idCalificador`) REFERENCES `usuario` (`idUsuario`),
+  ADD CONSTRAINT `calificacionusuario_ibfk_3` FOREIGN KEY (`idCalificado`) REFERENCES `usuario` (`idUsuario`);
+
+--
+-- Filtros para la tabla `comentariosproyecto`
+--
+ALTER TABLE `comentariosproyecto`
+  ADD CONSTRAINT `comentariosproyecto_ibfk_1` FOREIGN KEY (`idProyecto`) REFERENCES `proyecto` (`idProyecto`);
+
+--
+-- Filtros para la tabla `comentariosrelease`
+--
+ALTER TABLE `comentariosrelease`
+  ADD CONSTRAINT `comentariosrelease_ibfk_1` FOREIGN KEY (`idRelease`) REFERENCES `release` (`idRelease`);
+
+--
+-- Filtros para la tabla `participantes`
+--
+ALTER TABLE `participantes`
+  ADD CONSTRAINT `participantes_ibfk_1` FOREIGN KEY (`idProyecto`) REFERENCES `proyecto` (`idProyecto`),
+  ADD CONSTRAINT `participantes_ibfk_2` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`),
+  ADD CONSTRAINT `participantes_ibfk_3` FOREIGN KEY (`rol`) REFERENCES `rol` (`idRol`);
+
+--
+-- Filtros para la tabla `proyecto`
+--
+ALTER TABLE `proyecto`
+  ADD CONSTRAINT `proyecto_ibfk_1` FOREIGN KEY (`propietario`) REFERENCES `usuario` (`idUsuario`),
+  ADD CONSTRAINT `proyecto_ibfk_2` FOREIGN KEY (`estado`) REFERENCES `estado` (`idEstado`);
+
+--
+-- Filtros para la tabla `release`
+--
+ALTER TABLE `release`
+  ADD CONSTRAINT `release_ibfk_1` FOREIGN KEY (`estado`) REFERENCES `estado` (`idEstado`),
+  ADD CONSTRAINT `release_ibfk_2` FOREIGN KEY (`idProyecto`) REFERENCES `proyecto` (`idProyecto`);
+
+--
+-- Filtros para la tabla `tarea`
+--
+ALTER TABLE `tarea`
+  ADD CONSTRAINT `tarea_ibfk_1` FOREIGN KEY (`idProyecto`) REFERENCES `proyecto` (`idProyecto`),
+  ADD CONSTRAINT `tarea_ibfk_3` FOREIGN KEY (`estado`) REFERENCES `estado` (`idEstado`);
+
+--
+-- Filtros para la tabla `tareasrelease`
+--
+ALTER TABLE `tareasrelease`
+  ADD CONSTRAINT `tareasrelease_ibfk_1` FOREIGN KEY (`idRelease`) REFERENCES `release` (`idRelease`),
+  ADD CONSTRAINT `tareasrelease_ibfk_2` FOREIGN KEY (`idTarea`) REFERENCES `tarea` (`idTarea`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ workTeams/agregarHistoria/agregarHistoria.js
@@ -0,0 +1,170 @@
+
+$(document).ready(function (){
+    validarsesion();
+    cargarHistorias();
+    $("#agregar").click(function(){
+        let band = true;
+        if($("#inputNombreT").val().length<1){
+            $("#vNombreT").attr("class","text-danger visible");
+            $("#inputNombreT").attr("class","form-control is-invalid");
+            band = false;    
+        }
+        if($("#inputNumT").val().length<1){
+            $("#vNumT").attr("class","text-danger visible");
+            $("#inputNumT").attr("class","form-control is-invalid");
+            band = false;    
+        }
+        if($("#inputDesc").val().length<1){
+            $("#vDescT").attr("class","text-danger visible");
+            $("#inputDesc").attr("class","form-control is-invalid");
+            band = false;    
+        }
+        if($("#inputPond").val().length<1){
+            $("#vPondT").attr("class","text-danger visible");
+            $("#inputPond").attr("class","form-control is-invalid");
+            band = false;    
+        }
+
+        if(band){
+            agregarHistoria();
+        }
+    });
+
+    $("#formulario").keyup(function(){
+        $("#vNombreT").attr("class","invisible");
+        $("#inputNombreT").attr("class","form-control");
+        $("#vNumT").attr("class","invisible");         
+        $("#inputNumT").attr("class","form-control");
+        $("#vDescT").attr("class","invisible");         
+        $("#inputDesc").attr("class","form-control");
+        $("#vPondT").attr("class","invisible");         
+        $("#inputPond").attr("class","form-control");  
+    })
+
+    //Funcion para inicializar boton de menu izquierdo
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+});
+
+//Valida si hay una sesion iniciada para redireccionar
+function validarsesion(){
+    if(!localStorage.getItem("Logueado")){
+        location.href ="../login/";
+    }
+}
+
+//Confirma y agrega la historia ingresada
+function agregarHistoria(){
+    let contenedor = document.createElement("div");
+    contenedor.setAttribute("class", "container p-3 my-3");
+
+    let titulo = document.createElement("p");
+    titulo.innerHTML = "¿Desea agregar la actividad "+document.getElementById("inputNombreT").value+"?";
+    contenedor.appendChild(titulo);
+
+    //Inicio de la Alerta
+    alertify.confirm("Confirmación de Tarea", contenedor, function(){
+        let idProyecto = localStorage.getItem("proyectoSeleccionado");
+        let nombreT = document.getElementById("inputNombreT").value;
+        let numT = document.getElementById("inputNumT").value;
+        let descT = document.getElementById("inputDesc").value;
+        let pondT = document.getElementById("inputPond").value;
+        $.ajax({
+            method: "post",
+            url: "../logica.php",
+            data: {"idProyectoAH": idProyecto, "nombreT": nombreT, "numT": numT, "descT": descT, "pondT":pondT}
+        }).done(function(respuesta){
+            console.log(respuesta);
+            if(respuesta){
+                cargarHistorias();
+                document.getElementById("inputNombreT").value="";
+                document.getElementById("inputNumT").value="";
+                document.getElementById("inputDesc").value="";
+                document.getElementById("inputPond").value="";
+                alertify.success('Se agrego la Tarea');
+            }else{
+                alertify.error("Error al guardar tarea");
+            }
+        });
+    },function(){
+        alertify.error('No se agrego la Tarea');
+    }).set({labels:{ok:'Aceptar', cancel: 'Cancelar'}, padding: false});
+}
+
+function cargarHistorias(){
+    eliminarHistorias();
+    let idProyectoCH = localStorage.getItem("proyectoSeleccionado");
+    $.ajax({
+        method: "post",
+        url: "../logica.php",
+        data: {"idProyectoCH": idProyectoCH}
+    }).done(function(respuesta){
+        let array = JSON.parse(respuesta);
+        let cantP = 0;
+        console.log(array.length); 
+        for(i=0;i<array.length;i++){
+            cantP += parseInt(array[i]['puntos']);
+            $("#historias").append(`
+                <div class="card border-primary mb-3" style="width: 45%; float: left; margin-left:10px;">
+                    <div class="card-header bg-primary text-white">${array[i].nombreT}</div>
+                    <div class="card-body text-secondary">
+                    <h5 class="card-title">N° Historia: ${array[i].numT} | Puntos: ${array[i].puntos}</h5>
+                    <p class="card-text">${array[i].descripcion}</p>
+                    </div>
+                </div>
+            `);
+        }
+        cargarAvisos(array.length, cantP);
+    });
+}
+
+function eliminarHistorias(){
+    let element = document.getElementById("historias");
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+}
+
+function cargarAvisos(cantidad, cantP){
+    let idProyectoCA = localStorage.getItem("proyectoSeleccionado");
+    $.ajax({
+        method: "post",
+        url: "../logica.php",
+        data: {"idProyectoCA": idProyectoCA}
+    }).done(function(respuesta){
+        let array = JSON.parse(respuesta);
+        console.log(array);
+        //Convertir fecha inicial
+        let aFecha1 = (array[0]['fechaInicio']).split("/");
+        let fechaInicio = new Date(aFecha1[2]+"/"+aFecha1[1]+"/"+aFecha1[0]);
+        console.log(fechaInicio);
+        //Convertir fecha final
+        let aFecha2 = (array[0]['fechaFin']).split("/");
+        let fechaFin = new Date(aFecha2[2]+"/"+aFecha2[1]+"/"+aFecha2[0]);
+        console.log(fechaFin);
+        //Numero de Release
+        let numRelease = array[0]['numRelease'];
+        console.log(numRelease);
+        //Diferencia
+        let dias = fechaFin - fechaInicio;
+        let diff = dias/(1000 * 60 * 60 * 24);
+        console.log(diff, cantP);
+        $("#espacioDetalles").append(`
+            <br>
+            <div class="card border-success mb-3" t style="width: 93.5%; float: left; margin-left:10px;">
+                <div class="card-header bg-success border-success text-white">Información Release</div>
+                <div class="card-body text-secondary">
+                <h5 class="card-title">Definición del Trabajo</h5>
+                <p style="text-align: justify;" class="card-text">Se sugiere que dadas las ${cantidad} historias de usuario, presentes en el proyecto, y con una espectativa de ${numRelease} releases, se debe realizar un release cada ${parseInt(diff/numRelease)} días, y a su vez, en cada release efectuar ${Math.round(cantP/numRelease)+1} puntos de historia.</p>
+                </div>
+            </div>
+        `);
+    });
+}
+
+function cerrarSesion(){
+    localStorage.clear();
+    location.reload();
+}
